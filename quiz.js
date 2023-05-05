36a59aefd10adb54e70b17358bdb29d5e5bf38b2
bmd017 <70355930+bmd017@users.noreply.github.com>
2023-05-05 01:39:36 -0500

    Version 3
    Still needs number correct/total

--- quiz.js
+++ quiz.js
@@ -1,79 +1,145 @@
 document.addEventListener('DOMContentLoaded', async () => {
-  const txtFileURL = 'quiz1.txt'; // Replace with your .txt file URL
-  const quizData = await parseTxtFile(txtFileURL);
+  try {
+    const txtFileURL = './quiz1.txt';
+    const answerKeyURL = './answer_key.txt';
+    const quizData = await parseTxtFile(txtFileURL, answerKeyURL);
 
-  const container = document.getElementById('quiz-container');
-  const questionElement = document.getElementById('question');
-  const choicesContainer = document.getElementById('choices');
-  const resultsContainer = document.getElementById('results');
-  const restartButton = document.getElementById('restart');
+    const container = document.getElementById('quiz-container');
+    const questionElement = document.getElementById('question');
+    const choicesContainer = document.getElementById('choices-container');
+    const resultsContainer = document.getElementById('results');
+    const restartButton = document.getElementById('restart');
 
-  let currentQuestion = 0;
-  let correctAnswers = 0;
+    let currentQuestion = 0;
+    let correctAnswers = 0;
+    let answeredQuestions = [];
 
-  function showQuestion() {
-    const questionData = quizData[currentQuestion];
-    questionElement.textContent = questionData.question;
+    showQuestion();
 
-    choicesContainer.innerHTML = '';
+    function showQuestion() {
+      const questionData = quizData[currentQuestion];
+      questionElement.textContent = questionData.question;
 
-    questionData.choices.forEach((choice, index) => {
-      const button = document.createElement('button');
-      button.textContent = choice;
-      button.onclick = () => handleAnswerClick(index);
-      choicesContainer.appendChild(button);
-    });
-  }
+      choicesContainer.innerHTML = '';
 
-  function handleAnswerClick(selectedIndex) {
-    if (selectedIndex === quizData[currentQuestion].correctAnswer) {
-      correctAnswers++;
+      questionData.choices.forEach((choice, index) => {
+        const button = document.createElement('button');
+        button.textContent = choice;
+        button.onclick = () => handleAnswerClick(index);
+        choicesContainer.appendChild(button);
+      });
     }
 
-    currentQuestion++;
-
-    if (currentQuestion < quizData.length) {
-      showQuestion();
-    } else {
-      showResults();
+    function handleAnswerClick(selectedIndex) {
+      answeredQuestions.push({
+        questionIndex: currentQuestion,
+        userAnswer: selectedIndex,
+        correctAnswer: quizData[currentQuestion].correctAnswer
+      });
+    
+      if (selectedIndex === quizData[currentQuestion].correctAnswer) {
+        correctAnswers++;
+      }
+    
+      currentQuestion++;
+    
+      if (currentQuestion < quizData.length) {
+        showQuestion();
+      } else {
+        showResults();
+      }
     }
-  }
-
-  function showResults() {
-    questionElement.style.display = 'none';
-    choicesContainer.style.display = 'none';
-    resultsContainer.textContent = `You scored ${correctAnswers} out of ${quizData.length} (${Math.round((correctAnswers / quizData.length) * 100)}%)`;
-    resultsContainer.style.display = 'block';
-    restartButton.style.display = 'block';
-
-    restartButton.onclick = () => {
-      currentQuestion = 0;
-      correctAnswers = 0;
-      questionElement.style.display = 'block';
-      choicesContainer.style.display = 'block';
-      resultsContainer.style.display = 'none';
-      restartButton.style.display = 'none';
-      showQuestion();
-    };
-  }
-
-  async function parseTxtFile(url) {
-    const response = await fetch(url);
-    const txtData = await response.text();
-
-    const lines = txtData.trim().split('\n');
-    const quizData = [];
-
-    for (let i = 0; i < lines.length; i += 6) {
-      const question = lines[i].slice(3).trim();
-      const choices = lines.slice(i + 1, i + 5).map(line => line.slice(2).trim());
-      const correctAnswer = lines[i + 5].trim().charCodeAt(0) - 65;
-
-      quizData.push({ question, choices, correctAnswer });
+    
+    function showResults() {
+      console.log('Showing results');
+      console.log('Answered questions:', answeredQuestions);
+    
+      questionElement.style.display = 'none';
+      choicesContainer.style.display = 'none';
+      resultsContainer.innerHTML = `You scored ${correctAnswers} out of ${quizData.length} (${Math.round((correctAnswers / quizData.length) * 100)}%)<br><br>`;
+      resultsContainer.style.display = 'block';
+      restartButton.style.display = 'block';
+    
+      const wrongAnswers = answeredQuestions.filter(answer => answer.userAnswer !== answer.correctAnswer);
+      console.log('Wrong answers:', wrongAnswers);
+      const wrongResultsContainer = document.createElement('div');
+      wrongResultsContainer.innerHTML = `<strong>You got ${wrongAnswers.length} questions wrong:</strong><br><br>`;
+      resultsContainer.appendChild(wrongResultsContainer);
+    
+      wrongAnswers.forEach(answer => {
+        const questionData = quizData[answer.questionIndex];
+        const questionText = questionData.question;
+        const userAnswer = questionData.choices[answer.userAnswer];
+        const correctAnswer = questionData.choices[answer.correctAnswer];
+    
+        const answerDetails = document.createElement('div');
+        answerDetails.innerHTML = `
+          <strong>Question:</strong> ${questionText}<br>
+          <strong>Your Answer:</strong> ${userAnswer}<br>
+          <strong>Correct Answer:</strong> ${correctAnswer}<br><br>
+        `;
+        resultsContainer.appendChild(answerDetails);
+      });
+    
+      restartButton.onclick = () => {
+        currentQuestion = 0;
+        correctAnswers = 0;
+        answeredQuestions = [];
+        questionElement.style.display = 'block';
+        choicesContainer.style.display = 'block';
+        resultsContainer.style.display = 'none';
+        restartButton.style.display = 'none';
+        showQuestion();
+      };
     }
+    
 
-    return quizData;
+    async function parseTxtFile(url, answerKeyURL) {
+      const response = await fetch(url);
+      const txtData = await response.text();
+      const responseAnswers = await fetch(answerKeyURL);
+      const txtAnswers = await responseAnswers.text();
+    
+      const answers = txtAnswers
+        .trim()
+        .split('\n')
+        .filter(line => line.trim()) // Filter out empty lines
+        .map(line => line.slice(3).trim().toUpperCase());
+    
+      const lines = txtData.trim().split('\n');
+      const quizData = [];
+    
+      let i = 0;
+      while (i < lines.length) {
+        if (/^\d+\)/.test(lines[i])) { // Check if the line starts with a number followed by a closing parenthesis
+          const question = lines[i].slice(3).trim();
+          i++;
+    
+          const choices = [];
+          while (i < lines.length && !/^\d+\)/.test(lines[i])) { // Check if the line starts with a number followed by a closing parenthesis
+            if (/^[A-D]\)/.test(lines[i])) { // Check if the line starts with a capital letter (A-D) followed by a closing parenthesis
+              choices.push(lines[i].slice(2).trim());
+            }
+            i++;
+          }
+    
+          const answerIndex = quizData.length;
+          if (answerIndex < answers.length) {
+            const correctAnswer = answers[answerIndex].charCodeAt(0) - 65;
+            console.log('Current index:', answerIndex, 'Answer:', answers[answerIndex]);
+            quizData.push({ question, choices, correctAnswer });
+          } else {
+            console.warn('No corresponding answer found for question index:', answerIndex);
+          }
+        } else {
+          i++;
+        }
+      }
+    
+      return quizData;
+    }
+    
+  } catch (error) {
+    console.error('Error:', error);
   }
-
-  showQuestion();
 });
